f315f315cf60739e2db1d39184000f0e49b5bb09
Mahir <mahir@techwave.io>
2021-12-16 11:27:38 +0100

    Dex integration refactor

--- ads/vendors/dex.js
+++ ads/vendors/dex.js
@@ -6,21 +6,23 @@ import {loadScript, validateData} from '#3p/3p';
  */
 export function dex(global, data) {
   validateData(data, ['zoneid']);
-  data.url = '//adx.4strokemedia.com/www/delivery/asyncjs.php';
-  data.id = '6609ba8ff88606794e22ff360400bcea';
-  createIns(global, data);
-  loadScript(global, data.url);
+  loadScript(global, buildUrl(data));
 }
 
 /**
- * @param {!Window} global
  * @param {!Object} data
+ * @return {string}
  */
-function createIns(global, data) {
-  const ins = global.document.createElement('ins');
-  ins.setAttribute('data-revive-id', data.id);
-  ins.setAttribute('data-revive-zoneid', data['zoneid']);
-  ins.setAttribute('data-revive-videoid', data['videoid']);
-  ins.setAttribute('data-revive-audioid', data['audioid']);
-  global.document.getElementById('c').appendChild(ins);
+function buildUrl(data) {
+  let url = `https://adx.4strokemedia.com/www/delivery/ajs.php?zoneid=${data['zoneid']}`;
+
+  if (data['videoid']) {
+    url += `&videoid=${data['videoid']}`;
+  }
+
+  if (data['audioid']) {
+    url += `&audioid=${data['audioid']}`;
+  }
+
+  return url;
 }
